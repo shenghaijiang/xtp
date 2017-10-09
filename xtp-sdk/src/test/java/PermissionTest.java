c736e1161cd528d8823fb010255f5fa8866b697c
盛海江 <hailng08@163.com>
2017-10-09 11:05:11 +0800

    重复性验证

--- xtp-sdk/src/test/java/PermissionTest.java
+++ xtp-sdk/src/test/java/PermissionTest.java
@@ -10,16 +10,20 @@ import static org.junit.Assert.assertEquals;
  * Created by HaiL on 2017/5/11.
  */
 public class PermissionTest {
-
-    @Test
-    public void Test() {
-        Boolean r = PermissionUtil.IsAuthorized("eyJhbGciOiJIUzI1NiJ9NDkwMTUyNDk4LCJzd", 39, "A", "add");
-        assertEquals(true, r);
-    }
-
-    @Test
-    public void Test2() {
-        List<Map<String, Object>> r = PermissionUtil.IsData("eyJhbGciOiJIUzI1NiJ9NDkwMTUyNDk4LCJzd", 39, "用户管理");
-        assertEquals(true, r.size() > 0);
-    }
+//
+//    @Test
+//    public void name() throws Exception {
+//    }
+//
+//    @Test
+//    public void Test() {
+//        Boolean r = PermissionUtil.IsAuthorized("eyJhbGciOiJIUzI1NiJ9NDkwMTUyNDk4LCJzd", 39, "A", "add");
+//        assertEquals(true, r);
+//    }
+//
+//    @Test
+//    public void Test2() {
+//        List<Map<String, Object>> r = PermissionUtil.IsData("eyJhbGciOiJIUzI1NiJ9NDkwMTUyNDk4LCJzd", 39, "用户管理");
+//        assertEquals(true, r.size() > 0);
+//    }
 }
